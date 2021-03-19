bcac36f4cb91093b9de6e564d73e029d52fdf608
ibqn <evgen.ibqn@gmail.com>
2021-03-19 01:27:02 +0100

    add router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import { Home, Profile } from './pages'
+
 function App() {
-  return <div>app</div>
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
