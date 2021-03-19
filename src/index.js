0a8d1898b5d832bd93b59cb4bdf8f97f599ac86c
ibqn <evgen.ibqn@gmail.com>
2021-03-19 01:57:16 +0100

    add material core support

--- src/index.js
+++ src/index.js
@@ -3,9 +3,41 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import reportWebVitals from './reportWebVitals'
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import indigo from '@material-ui/core/colors/indigo'
+import orange from '@material-ui/core/colors/orange'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+// import material-ui fonts weights: 300,400,500,700
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: indigo[500],
+      main: indigo[700],
+      dark: indigo[900],
+    },
+    secondary: {
+      light: orange[300],
+      main: orange[500],
+      dark: orange[700],
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
