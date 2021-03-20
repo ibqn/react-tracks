84ee2077a4793e17174e1915c522860aa5e32c0d
ibqn <evgen.ibqn@gmail.com>
2021-03-20 23:49:40 +0100

    draw structure of home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import { useTheme, makeStyles } from '@material-ui/core/styles'
+
+import { SearchTrack, CreateTrack, TrackList } from '../components/track'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: theme.spacing(2),
+  },
+}))
 
 const Home = () => {
-  return <div>home</div>
+  const [search, setSearch] = useState(null)
+  const theme = useTheme()
+  const classes = useStyles(theme)
+
+  const tracks = []
+  return (
+    <div className={classes.container}>
+      <SearchTrack setSearch={setSearch} />
+      <CreateTrack />
+
+      <TrackList tracks={tracks} />
+    </div>
+  )
 }
 
 export default Home
