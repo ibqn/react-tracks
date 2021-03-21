ac2c786e374f64b92bd7be2f33a46683eec00ff3
ibqn <evgen.ibqn@gmail.com>
2021-03-21 11:40:11 +0100

    fix importing of shared components

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -1,5 +1,6 @@
 import Header from './header'
 import Error from './error'
 import AudioPlayer from './audio-player'
+import Loading from './loading'
 
-export { Header, Error, AudioPlayer }
+export { Header, Error, AudioPlayer, Loading }
