dc163df604c36a6d0c4bf192f87c7fdbd05f5271
ibqn <evgen.ibqn@gmail.com>
2021-03-22 14:11:04 +0100

    start adding like component

--- src/components/track/like-track.jsx
+++ src/components/track/like-track.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { useTheme, makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp'
 
-const LikeTrack = () => {
-  return <div>like track</div>
+const useStyles = makeStyles((theme) => ({
+  iconButton: {
+    color: 'deeppink',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    marginLeft: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
+  },
+}))
+
+const LikeTrack = ({ trackId }) => {
+  const theme = useTheme()
+  const classes = useStyles(theme)
+
+  const likeTrack = () => {}
+
+  const alreadyLiked = () => false
+
+  const likeCount = 3
+
+  return (
+    <IconButton
+      onClick={likeTrack}
+      className={classes.iconButton}
+      disabled={alreadyLiked()}
+    >
+      {likeCount}
+      <ThumbUpIcon className={classes.icon} />
+    </IconButton>
+  )
 }
 
 export default LikeTrack
