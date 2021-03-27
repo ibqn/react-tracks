4b931253683f75de1743a40763e21c2d56fbd9f5
ibqn <evgen.ibqn@gmail.com>
2021-03-28 00:28:05 +0100

    add basic shape of update track component

--- src/components/track/update-track.jsx
+++ src/components/track/update-track.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { useState } from 'react'
+
+import IconButton from '@material-ui/core/IconButton'
+import EditIcon from '@material-ui/icons/Edit'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({}))
 
 const UpdateTrack = () => {
-  return <div>update track</div>
+  const [open, setOpen] = useState(false)
+
+  const classes = useStyles()
+
+  return (
+    <IconButton onClick={() => setOpen(true)}>
+      <EditIcon />
+    </IconButton>
+  )
 }
 
 export default UpdateTrack
