51330804fc9d6c6221c07cba738fa5425ae6e392
ibqn <evgen.ibqn@gmail.com>
2021-03-27 13:46:09 +0100

    add fab floated action button to create new track component

--- src/components/track/create-track.jsx
+++ src/components/track/create-track.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { makeStyles /*useTheme*/ } from '@material-ui/core/styles'
+
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
+import ClearIcon from '@material-ui/icons/Clear'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  dialog: {
+    margin: '0 auto',
+    maxWidth: 550,
+  },
+  textField: {
+    margin: theme.spacing(),
+  },
+  cancel: {
+    color: 'red',
+  },
+  save: {
+    color: 'green',
+  },
+  button: {
+    margin: theme.spacing(),
+  },
+  icon: {
+    marginLeft: theme.spacing(),
+  },
+  input: {
+    display: 'none',
+  },
+  fab: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+    zIndex: '200',
+  },
+}))
 
 const CreateTrack = () => {
-  return <div>create new track</div>
+  // const theme = useTheme()
+  const classes = useStyles(/*theme*/)
+
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <Fab
+        onClick={() => setOpen(true)}
+        className={classes.fab}
+        color="secondary"
+      >
+        {open ? <ClearIcon /> : <AddIcon />}
+      </Fab>
+    </>
+  )
 }
 
 export default CreateTrack
