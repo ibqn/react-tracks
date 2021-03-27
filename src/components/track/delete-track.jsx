74788fbb764c4397fe7ff01197312eff1aa85775
ibqn <evgen.ibqn@gmail.com>
2021-03-28 00:19:25 +0100

    style delete track component

--- src/components/track/delete-track.jsx
+++ src/components/track/delete-track.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { gql, useMutation } from '@apollo/client'
 
-const DeleteTrack = () => {
-  return <div>delete track</div>
+import IconButton from '@material-ui/core/IconButton'
+import TrashIcon from '@material-ui/icons/DeleteForeverOutlined'
+
+import { useAuth } from '../auth'
+
+const DELETE_TRACK_MUTATION = gql`
+  mutation($trackId: Int!) {
+    deleteTrack(trackId: $trackId) {
+      trackId
+    }
+  }
+`
+
+const DeleteTrack = ({ track }) => {
+  const [deleteTrack, { loading, error }] = useMutation(DELETE_TRACK_MUTATION)
+
+  const { user } = useAuth()
+
+  const handleDelete = () => {
+    console.log('delete track', track.id)
+
+    if (user.id === track.postedBy.id) {
+      console.log('match', user.id)
+    }
+  }
+
+  return (
+    <IconButton onClick={handleDelete}>
+      <TrashIcon />
+    </IconButton>
+  )
 }
 
 export default DeleteTrack
