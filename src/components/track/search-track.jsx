36a3dc9c4333829cb5dc70bfadab2f95bad85884
ibqn <evgen.ibqn@gmail.com>
2021-03-28 00:27:42 +0100

    add basic shape of search track component

--- src/components/track/search-track.jsx
+++ src/components/track/search-track.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { useState } from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import ClearIcon from '@material-ui/icons/Clear'
+import Paper from '@material-ui/core/Paper'
+import IconButton from '@material-ui/core/IconButton'
+import SearchIcon from '@material-ui/icons/Search'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(0.25, 0.5), // padding: '2px 4px'
+    margin: theme.spacing(1, 0),
+    display: 'flex',
+    alignItems: 'center',
+  },
+}))
 
 const SearchTrack = () => {
-  return <div>search track</div>
+  const classes = useStyles()
+
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  const clearSearchInput = () => {}
+
+  const handleSearchInput = () => {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Paper className={classes.root} elevation={1}>
+        <IconButton onClick={clearSearchInput}>
+          <ClearIcon />
+        </IconButton>
+        <TextField
+          fullWidth
+          placeholder="Search All Tracks"
+          InputProps={{
+            disableUnderline: true,
+          }}
+          onChange={handleSearchInput}
+        />
+        <IconButton type="submit">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </form>
+  )
 }
 
 export default SearchTrack
