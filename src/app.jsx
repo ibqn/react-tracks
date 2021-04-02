89e58a0196e30d000eba77344a05cb85468f9de3
ibqn <evgen.ibqn@gmail.com>
2021-04-02 21:40:05 +0200

    remove redundant div tag

--- src/app.jsx
+++ src/app.jsx
@@ -4,26 +4,24 @@ import { Home, Profile } from './pages'
 import { Login, Register, ProtectedRoute } from './components/auth'
 import { Header } from './components/shared'
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <ProtectedRoute path="/profile/:username">
-            <Profile />
-          </ProtectedRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Header />
+      <Switch>
+        <ProtectedRoute path="/profile/:username">
+          <Profile />
+        </ProtectedRoute>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   )
 }
