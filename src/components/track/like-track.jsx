38bf29de000ecff1bfc47a55d96ea4cf1fc45763
ibqn <evgen.ibqn@gmail.com>
2021-08-19 14:58:18 +0200

    add jsconfig to use absolute imports

--- src/components/track/like-track.jsx
+++ src/components/track/like-track.jsx
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 import PropTypes from 'prop-types'
 
 import { gql, useMutation } from '@apollo/client'
-import { UPDATE_TRACKS } from '../../fragments'
+import { UPDATE_TRACKS } from 'fragments'
 
 import { makeStyles } from '@material-ui/core/styles'
 
